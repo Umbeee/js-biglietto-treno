24887bce144c4cd21b0d0a89794e4b773aad8805
Umbeee <umberto.nesta@hotmail.com>
2023-03-22 14:28:06 +0100

    esercizio base completato, da sostituire prompt con input

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,25 @@
 // va applicato uno sconto del 20% per i minorenni
 // va applicato uno sconto del 40% per gli over 65.
 // L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+
+
+//document.writeln('<p> hello world </p>');
+const prezzoAlKm = 0.21;
+
+let numeroKm = prompt('Quanti km deve percorrere il passeggero?')
+let eta = prompt('inserire età del passeggero')
+
+let costo = numeroKm * prezzoAlKm;
+
+if(eta > 65){
+    costo = costo * 0.6;
+    costo = costo.toFixed(2);
+    document.writeln(`<p> Il prezzo del suo biglietto sarà di ${costo} euro, prezzo scontato del 40% per via della promo over65</p>`);
+}else if(eta < 18){
+    costo = costo * 0.8;
+    costo = costo.toFixed(2);
+    document.writeln(`<p> Il prezzo del suo biglietto sarà di ${costo} euro, prezzo scontato del 20% per via della promo under18</p>`)
+}else{
+    costo = costo.toFixed(2);
+    document.writeln(`<p> Il prezzo del suo biglietto sarà di ${costo} euro</p>`)
+}
